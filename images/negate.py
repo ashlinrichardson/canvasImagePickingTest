af421af8d123df482619e2f59f4218fb738a2d0b
ashlinrichardson <ashlin.richardson@gmail.com>
2016-10-01 16:04:21 -0700

    Changed the images to be more professional.

--- images/negate.py
+++ images/negate.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os,sys
+a = os.system('find ./ -name "*inverte*.jpg" | xargs rm'); 
 files = os.popen("ls -1 image*.jpg").readlines();
 for file in files:
   a = os.system("convert -negate "+file.strip()+" "+file.strip()+"-inverted.jpg");
